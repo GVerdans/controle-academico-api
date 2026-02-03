0f1f4ceea2449e7c90a3c6391f76ebb39ee1ffad
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-02-03 17:31:21 -0300

    include endpoint create material and route

--- src/controllers/materias.controller.js
+++ src/controllers/materias.controller.js
@@ -11,7 +11,32 @@ class MateriasController {
         }
     }
 
-    async create(req, res) {}
+    async create(req, res) {
+        const { id_periodo, nome, nota_1, nota_2, media, status } = req.body;
+
+        try {
+            if (!id_periodo || !nome) {
+                res.status(400).json({
+                    error: "Please insert id_periodo and nome !",
+                });
+            }
+
+            const newMateria = await MateriasRepository.create({
+                id_periodo,
+                nome,
+                nota_1,
+                nota_2,
+                media,
+                status,
+            });
+
+            return res
+                .status(201)
+                .json({ success: "Materia created !", newMateria });
+        } catch (e) {
+            return res.json({ error: "Failed to create Materia !", e });
+        }
+    }
 
     // async update(res, res) {}
 
--- src/routes/materias.routes.js
+++ src/routes/materias.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const MateriasController = require("../controllers/materias.controller");
 
 router.get("/", MateriasController.list);
-// router.post("/", materiasController.create);
+router.post("/", MateriasController.create);
 // router.put("/:id", materiasController.update);
 // router.delete("/:id", materiasController.remove);
 
