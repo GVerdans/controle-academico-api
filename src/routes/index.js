e67f1529d08559e86605292d153a4edc359d11ba
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-02-04 19:18:17 -0300

    forgot change branch T-T

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const materiasRoutes = require("./materias.routes");
+const usersRoutes = require("./users.routes");
 
 router.get("/", (req, res) => {
     res.json({ status: "ON" });
 });
 
 router.use("/materias", materiasRoutes);
+router.use("/users", usersRoutes);
 
 module.exports = router;
