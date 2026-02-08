3097d2af6eae99ec8b1e6071d8c5eca7a5654b50
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-02-08 18:48:38 -0300

    create auth middleware to materias routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const materiasRoutes = require("./materias.routes");
 const usersRoutes = require("./users.routes");
 const authRoutes = require("./auth.routes");
 
+const authMiddleware = require("../middlewares/auth.middleware");
+
 router.get("/", (req, res) => {
     res.json({ status: "ON" });
 });
 
-router.use("/materias", materiasRoutes);
+router.use("/materias", authMiddleware, materiasRoutes);
 router.use("/users", usersRoutes);
 router.use("/auth", authRoutes);
 
