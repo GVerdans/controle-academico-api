94b154e831d50b5a3702192b797172c0ff4b2bb2
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-02-16 16:43:29 -0300

    update variables for railway again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.MYSQLPORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`http://localhost:${PORT}`);
