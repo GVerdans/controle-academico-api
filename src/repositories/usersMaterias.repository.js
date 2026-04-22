bc1bce0cff18cddcb4d67ba07d6f9928524929b1
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-04-22 17:55:23 -0300

    bugfix/ajustes para PgSQL

--- src/repositories/usersMaterias.repository.js
+++ src/repositories/usersMaterias.repository.js
@@ -3,49 +3,50 @@ const db = require("../config/database");
 class UsersMateriasRepository {
     async enroll(userId, materiaId) {
         let status = "cursando";
-        const [result] = await db.query(
+
+        const result = await db.query(
             `
-            INSERT INTO users_materias (id_user, id_materia, status)
-            VALUES($1, $2, $3)
-            RETURNING id_matricula
-            `,
+      INSERT INTO users_materias (id_user, id_materia, status)
+      VALUES($1, $2, $3)
+      RETURNING id_matricula
+      `,
             [userId, materiaId, status],
         );
 
-        return result[0].id_matricula;
+        return result.rows[0].id_matricula;
     }
 
     async findByUser(userId) {
-        const [rows] = await db.query(
+        const result = await db.query(
             `
-            SELECT 
-                um.id_matricula,
-                m.id_materia,
-                m.nome,
-                um.nota_1,
-                um.nota_2,
-                um.media,
-                um.status
-            FROM users_materias um
-            JOIN materias m ON m.id_materia = um.id_materia
-            WHERE um.id_user = $1
-            `,
+      SELECT 
+        um.id_matricula,
+        m.id_materia,
+        m.nome,
+        um.nota_1,
+        um.nota_2,
+        um.media,
+        um.status
+      FROM users_materias um
+      JOIN materias m ON m.id_materia = um.id_materia
+      WHERE um.id_user = $1
+      `,
             [userId],
         );
 
-        return rows;
+        return result.rows;
     }
 
     async updateGrades(userId, id_matricula, nota_1, nota_2) {
         const media = (Number(nota_1) + Number(nota_2)) / 2;
         let status = media >= 6 ? "aprovado" : "reprovado";
 
-        const [result] = await db.query(
+        const result = await db.query(
             `
-            UPDATE users_materias
-            SET nota_1 = $1, nota_2 = $2, media = $3, status = $4
-            WHERE id_matricula = $5 AND id_user = $6
-            `,
+      UPDATE users_materias
+      SET nota_1 = $1, nota_2 = $2, media = $3, status = $4
+      WHERE id_matricula = $5 AND id_user = $6
+      `,
             [nota_1, nota_2, media, status, id_matricula, userId],
         );
 
